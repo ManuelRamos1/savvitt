b33ad9431cb3699bbee504304e08711b2ba75e93
Heriberto Valdez Carranza <142847261+HeribertoValdez@users.noreply.github.com>
2023-12-14 11:08:51 -0800

    RUTEO
    creacion de sistemarespaldos con ruteo se agrega un acarpeta debajo de routes y despues +pages.svelte para llamar la carpeta en los links

--- src/routes/sistemarespaldos.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-    import Navbar from "../Navbar.svelte";
-
-</script>
-<Navbar/>
-
-<h1>este es el modulo repaldos</h1>
---
+++ src/routes/sistemarespaldos/+page.svelte
@@ -0,0 +1,106 @@
+<script>
+    import Navbar from "../../Navbar.svelte";
+
+</script>
+<Navbar/>
+<style>
+    body {
+        margin: 0; /* Elimina márgenes del cuerpo */
+        padding: 0; /* Elimina relleno del cuerpo */
+    }
+    
+    .navbar {
+        margin-bottom: 0 !important; /* Elimina el margen inferior del navbar */
+    }
+
+    .relative {
+        margin: 0; /* Elimina márgenes del bloque principal */
+        padding: 0; /* Elimina relleno del bloque principal */
+    }
+</style>
+
+<div class="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
+    <img src="https://images.unsplash.com/photo-1521737604893-d14cc237f11d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&crop=focalpoint&fp-y=.8&w=2830&h=1500&q=80&blend=111827&sat=-100&exp=15&blend-mode=multiply" alt="" class="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center">
+    <div class="hidden sm:absolute sm:-top-10 sm:right-1/2 sm:-z-10 sm:mr-10 sm:block sm:transform-gpu sm:blur-3xl" aria-hidden="true">
+      <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+    <div class="absolute -top-52 left-1/2 -z-10 -translate-x-1/2 transform-gpu blur-3xl sm:top-[-28rem] sm:ml-16 sm:translate-x-0 sm:transform-gpu" aria-hidden="true">
+      <div class="aspect-[1097/845] w-[68.5625rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20" style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"></div>
+    </div>
+    <div class="mx-auto max-w-7xl px-6 lg:px-8">
+      <div class="mx-auto max-w-2xl lg:mx-0">
+        <h2 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">Etas protegido contra Ramsomeware?</h2>
+        <p class="mt-6 text-lg leading-8 text-gray-300">
+            "Optimiza la seguridad de tu empresa con nuestra especialización en implementación de sistemas Synology. Confía en nosotros para respaldos eficientes y la protección de tus datos críticos."</p>
+      </div>
+      <div class="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
+     
+        <dl class="mt-16 grid grid-cols-1 gap-8 sm:mt-20 sm:grid-cols-2 lg:grid-cols-4">
+          <div class="flex flex-col-reverse">
+            <dt class="text-base leading-7 text-gray-300">Offices worldwide</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">12</dd>
+          </div>
+          <div class="flex flex-col-reverse">
+            <dt class="text-base leading-7 text-gray-300">Full-time colleagues</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">300+</dd>
+          </div>
+          <div class="flex flex-col-reverse">
+            <dt class="text-base leading-7 text-gray-300">Hours per week</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">40</dd>
+          </div>
+          <div class="flex flex-col-reverse">
+            <dt class="text-base leading-7 text-gray-300">Paid time off</dt>
+            <dd class="text-2xl font-bold leading-9 tracking-tight text-white">Unlimited</dd>
+          </div>
+        </dl>
+      </div>
+    </div>
+  </div>
+
+
+
+
+  <div class="bg-white">
+    <div class="mx-auto grid max-w-2xl grid-cols-1 items-center gap-x-8 gap-y-16 px-4 py-24 sm:px-6 sm:py-32 lg:max-w-7xl lg:grid-cols-2 lg:px-8">
+      <div>
+        <h2 class="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Technical Specifications</h2>
+        <p class="mt-4 text-gray-500">The walnut wood card tray is precision milled to perfectly fit a stack of Focus cards. The powder coated steel divider separates active cards from new ones, or can be used to archive important task lists.</p>
+  
+        <dl class="mt-16 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 sm:gap-y-16 lg:gap-x-8">
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Origin</dt>
+            <dd class="mt-2 text-sm text-gray-500">Designed by Good Goods, Inc.</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Material</dt>
+            <dd class="mt-2 text-sm text-gray-500">Solid walnut base with rare earth magnets and powder coated steel card cover</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Dimensions</dt>
+            <dd class="mt-2 text-sm text-gray-500">6.25&quot; x 3.55&quot; x 1.15&quot;</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Finish</dt>
+            <dd class="mt-2 text-sm text-gray-500">Hand sanded and finished with natural oil</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Includes</dt>
+            <dd class="mt-2 text-sm text-gray-500">Wood card tray and 3 refill packs</dd>
+          </div>
+          <div class="border-t border-gray-200 pt-4">
+            <dt class="font-medium text-gray-900">Considerations</dt>
+            <dd class="mt-2 text-sm text-gray-500">Made from natural materials. Grain and color vary with each item.</dd>
+          </div>
+        </dl>
+      </div>
+      <div class="grid grid-cols-2 grid-rows-2 gap-4 sm:gap-6 lg:gap-8">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-01.jpg" alt="Walnut card tray with white powder coated steel divider and 3 punchout holes." class="rounded-lg bg-gray-100">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-02.jpg" alt="Top down view of walnut card tray with embedded magnets and card groove." class="rounded-lg bg-gray-100">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-03.jpg" alt="Side of walnut card tray with card groove and recessed card area." class="rounded-lg bg-gray-100">
+        <img src="https://tailwindui.com/img/ecommerce-images/product-feature-03-detail-04.jpg" alt="Walnut card tray filled with cards and card angled in dedicated groove." class="rounded-lg bg-gray-100">
+      </div>
+    </div>
+  </div>
+  
+  
+  
